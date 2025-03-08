3dc68079a59b4ae55d627f5c547eaaca6d66481c
tompernet2 <tom.pernet2@gmail.com>
2025-03-08 23:59:47 +0100

    r
    r

--- scriptUnProjet.js
+++ scriptUnProjet.js
@@ -6,28 +6,36 @@ document.addEventListener("DOMContentLoaded", function () {
     
     let index = 0;
     
-    const imageWidth = images[0].clientWidth + 30 * 2; // Largeur de l'image + marges
+    const imageWidth = images[0].clientWidth + 30 * 2; 
     
+    const updateBtn = () => {
+        prevButton.disabled = index === 0;
+        nextButton.disabled = index === images.length - 1;
+    };
+
     nextButton.addEventListener("click", function () {
         if (index < images.length - 1) {
             index++;
             images.forEach(img => {
                 img.style.transform = `translateX(-${index * imageWidth}px)`;
             });
+            updateBtn();
         }
     });
-    
+
     prevButton.addEventListener("click", function () {
         if (index > 0) {
             index--;
             images.forEach(img => {
                 img.style.transform = `translateX(-${index * imageWidth}px)`;
             });
+            updateBtn();
         }
     });
-    
-    // Ajoute une transition pour un effet fluide
+
     images.forEach(img => {
         img.style.transition = "transform 0.5s ease-in-out";
     });
+
+    updateBtn();
 });
